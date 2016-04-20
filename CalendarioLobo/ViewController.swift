a502f78eaa0f35a7789b473c3a2b924bb1f28ede
Alexander Rivera Rivera <arivepr@outlook.com>
2016-04-19 23:35:37 -0400

    Initial Commit

---
+++ CalendarioLobo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalendarioLobo
+//
+//  Created by Alexander Rivera on 4/19/16.
+//  Copyright © 2016 Alexander Rivera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
